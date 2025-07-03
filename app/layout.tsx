085d3c1fd8fe40194668203d76bedc137a49f9e0
JM <josephma.kw@gmail.com>
2025-07-03 10:49:05 -0600

    addng root file

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+// app/layout.tsx
+import "./globals.css";
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900">{children}</body>
+    </html>
+  );
+}
