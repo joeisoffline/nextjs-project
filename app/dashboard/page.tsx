943ad8c20fc67c01f400fb0b3495fa347396cf73
JM <josephma.kw@gmail.com>
2025-07-25 15:46:58 -0700

    changed up verbage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default async function Dashboard() {
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.email) {
-    return <p>Sign in</p>;
+    return <p>Sign in/Sign up</p>;
   }
 
   const customers = await prisma.customer.findMany({
