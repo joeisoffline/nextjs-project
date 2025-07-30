9ae446c47d966f6f2bcca04dee1d7b1060a915dd
JM <josephma.kw@gmail.com>
2025-07-30 10:36:35 -0600

    updates

--- components/CustomerForm.tsx
+++ components/CustomerForm.tsx
@@ -5,7 +5,7 @@ export default function CustomerForm() {
     <form className="space-y-4">
       <input type="text" name="name" placeholder="Customer name" className="w-full p-2 border rounded" />
       <input type="email" name="email" placeholder="Email address" className="w-full p-2 border rounded" />
-      <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">Add Customer</button>
+      <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">Add/Remove Customer</button>
     </form>
   );
 }
